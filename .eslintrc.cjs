d8b9e9a7af429853789e971b807866378248c306
2wndrhs <2wndrhs@gmail.com>
2024-01-09 20:23:56 +0900

    chore: 테스트 환경 구축

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
