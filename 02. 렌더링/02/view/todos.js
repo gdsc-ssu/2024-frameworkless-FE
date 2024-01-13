6fc02b72b5b5d0513a8a7f3ff6781bdcf01fa9d7
fecapark <fecapark@gmail.com>
2024-01-13 21:01:08 +0900

    feat: todos.js 구현 완료

---
+++ 02. 렌더링/02/view/todos.js
@@ -0,0 +1,44 @@
+import { createInnerHTML } from './utils';
+
+const getTodoItemElement = ({ text, completed }) => {
+  const toggle = createInnerHTML('input', {
+    attributes: {
+      class: 'toggle',
+      type: 'checkbox',
+      checked: completed,
+    },
+  });
+  const label = createInnerHTML('label', {
+    innerHTML: text,
+  });
+  const edit = createInnerHTML('input', {
+    attributes: {
+      class: 'edit',
+      value: text,
+    },
+  });
+  const viewBox = createInnerHTML('div', {
+    attributes: {
+      class: 'view',
+    },
+    innerHTML: toggle + label,
+  });
+
+  return createInnerHTML('li', {
+    attributes: {
+      class: completed ? 'completed' : '',
+    },
+    innerHTML: `${viewBox}${edit}`,
+  });
+};
+
+export default (targetElement, state) => {
+  const { todos } = state;
+
+  const innerHTML = todos.map((todo) => getTodoItemElement(todo)).join('');
+
+  const newElement = targetElement.cloneNode(true);
+  newElement.innerHTML = innerHTML;
+
+  return newElement;
+};
