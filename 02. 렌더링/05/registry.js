6e1236a3ac1c91e6af2ed780221251f7b4c0d129
Hanna922 <hanna.urssu@gmail.com>
2024-01-20 00:00:18 +0900

    feat: add renderRoot

--- 02. 렌더링/05/registry.js
+++ 02. 렌더링/05/registry.js
@@ -1,6 +1,38 @@
-const registry = {}; // export 하지 말아주세요!
+import todosView from './view/todos';
+import counterView from './view/counter';
+import filtersView from './view/filters';
 
-const add = () => {};
-const renderRoot = () => {};
+const registry = {
+  todos: todosView,
+  counter: counterView,
+  filters: filtersView,
+};
+
+const renderWrapper = (component) => (targetElement, state) => {
+  const element = component(targetElement, state);
+
+  const childComponent = element.querySelectorAll('[data-component]');
+
+  Array.from(childComponent).forEach((v) => {
+    const name = v.dataset.component;
+    const child = registry[name];
+    if (!child) {
+      return;
+    }
+
+    v.replaceWith(child(v, state));
+  });
+
+  return element;
+};
+
+const add = (name, component) => {
+  registry[name] = renderWrapper(component);
+};
+
+const renderRoot = (root, state) => {
+  const clonComponent = (element) => element.cloneNode(true);
+  return renderWrapper(clonComponent)(root, state);
+};
 
 export { add, renderRoot };
