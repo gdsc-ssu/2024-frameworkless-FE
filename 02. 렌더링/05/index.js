c454679074baa56dcbd7d03502cdbfaefae176ef
Hanna922 <hanna.urssu@gmail.com>
2024-01-20 00:00:02 +0900

    feat: add dynamic data rendering

--- 02. 렌더링/05/index.js
+++ 02. 렌더링/05/index.js
@@ -1,14 +1,23 @@
+import applyDiff from './applyDiff.js';
 import getTodos from './getTodos.js';
+import { renderRoot } from './registry.js';
 
 const state = {
   todos: getTodos(),
   currentFilter: 'All',
 };
 
-const main = document.querySelector('.todoapp');
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const main = document.querySelector('.todoapp');
+    const newMain = renderRoot(main, state);
+    applyDiff(document.body, main, newMain);
+  });
+};
 
-window.requestAnimationFrame(() => {
-  // appView() 대신 registry.js 의 코드를 활용해보세요.
-  // const newMain = appView(main, state);
-  // main.replaceWith(newMain);
-});
+window.setInterval(() => {
+  state.todos = getTodos();
+  render();
+}, 1000);
+
+render();
