6f8c927dd64fab194aa7a6bc42a5d74b68fb06aa
Hanna922 <hanna.urssu@gmail.com>
2024-01-19 23:58:20 +0900

    feat: add applyDiff func

--- 02. 렌더링/05/applyDiff.js
+++ 02. 렌더링/05/applyDiff.js
@@ -1,3 +1,27 @@
-const applyDiff = () => {};
+const applyDiff = (parentNode, realNode, virtualNode) => {
+  if (realNode && !virtualNode) {
+    realNode.remove();
+    return;
+  }
+
+  if (!realNode && virtualNode) {
+    parentNode.appendChild(virtualNode);
+    return;
+  }
+
+  if (realNode.tagName !== virtualNode.tagName) {
+    realNode.replaceWith(virtualNode);
+    return;
+  }
+
+  const realChildren = Array.from(realNode.childNodes);
+  const virtualChildren = Array.from(virtualNode.childNodes);
+
+  const max = Math.max(realChildren.length, virtualChildren.length);
+
+  for (let i = 0; i < max; i += 1) {
+    applyDiff(realNode, realChildren[i], virtualChildren[i]);
+  }
+};
 
 export default applyDiff;
