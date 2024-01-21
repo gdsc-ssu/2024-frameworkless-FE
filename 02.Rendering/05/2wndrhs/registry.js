5eff5dd18256f4f6089e45d309c45a7ed1c821fd
2wndrhs <2wndrhs@gmail.com>
2024-01-22 07:08:49 +0900

    fix(02/05): `state`를 반영할 수 있게 `renderWrapper()` 함수를 수정

--- 02.Rendering/05/2wndrhs/registry.js
+++ 02.Rendering/05/2wndrhs/registry.js
@@ -1,8 +1,8 @@
 const registry = {}; // export 하지 말아주세요!
 
 // renderWrapper 함수가 반환하는 함수는 클로저가 되어 component 함수를 기억
-const renderWrapper = (component) => (targetElement) => {
-  const element = component(targetElement);
+const renderWrapper = (component) => (targetElement, state) => {
+  const element = component(targetElement, state);
 
   const childComponents = element.querySelectorAll('[data-component]');
 
@@ -10,7 +10,7 @@ const renderWrapper = (component) => (targetElement) => {
     const name = target.dataset.component;
     const child = registry[name];
     // child(target) 함수는 새로운 DOM 요소를 반환
-    target.replaceWith(child(target));
+    target.replaceWith(child(target, state));
   });
 
   return element;
