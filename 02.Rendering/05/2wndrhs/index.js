214a4f5788ea4181e65f26a953ca65d34db8bd2e
2wndrhs <2wndrhs@gmail.com>
2024-01-22 06:10:37 +0900

    feat(02/05): 렌더링 함수 구현

--- 02.Rendering/05/2wndrhs/index.js
+++ 02.Rendering/05/2wndrhs/index.js
@@ -1,5 +1,9 @@
-/* eslint-disable no-unused-vars */
 import getTodos from './getTodos.js';
+// eslint-disable-next-line no-unused-vars
+import { add, renderRoot } from './registry.js';
+import applyDiff from './applyDiff.js';
+
+const filters = ['All', 'Active', 'Completed'];
 
 const state = {
   todos: getTodos(),
@@ -8,8 +12,16 @@ const state = {
 
 const main = document.querySelector('.todoapp');
 
-window.requestAnimationFrame(() => {
+const render = window.requestAnimationFrame(() => {
+  const newMain = renderRoot(main, state);
+  applyDiff(document.body, main, newMain);
   // appView() 대신 registry.js 의 코드를 활용해보세요.
   // const newMain = appView(main, state);
   // main.replaceWith(newMain);
 });
+
+window.setInterval(() => {
+  state.todos = getTodos();
+  state.currentFilter = filters[Math.floor(Math.random() * 3)];
+  render();
+}, 5000);
