81f54922b89d1afa84a6fda80b9cc04b36cfc6d3
SuJin <kimsujin001127@gmail.com>
2024-01-25 16:39:33 +0900

    feat: isNodeChanged.js

--- 02.Rendering/05/Sujin/isNodeChanged.js
+++ 02.Rendering/05/Sujin/isNodeChanged.js
@@ -1,3 +1,33 @@
-const isNodeChanged = () => {};
+const isNodeChanged = (node1, node2) => {
+  const n1Attributes = node1.attributes;
+  const n2Attributes = node2.attributes;
+
+  // 속성 수가 다른지 확인
+  if (n1Attributes.length !== n2Attributes.length) {
+    return true;
+  }
+
+  // 하나 이상의 속성이 변경되었는지 확인
+  const differentAttribute = Array.from(n1Attributes).find((attribute) => {
+    const { name } = attribute;
+    const attribute1 = node1.getAttribute(name);
+    const attribute2 = node2.getAttribute(name);
+
+    return attribute1 !== attribute2;
+  });
+  if (differentAttribute) {
+    return true;
+  }
+
+  
+  if (    // 노드에는 자식이 없으며
+    node1.children.length === 0 &&
+    node2.children.length === 0 &&
+    node1.textContent !== node2.textContent   // textContent가 다르다
+  ) {
+    return true;
+  }
+  return false;
+};
 
 export default isNodeChanged;
