14141de1fefada77a10cc83f27e857f5a0b97fcb
SuJin <kimsujin001127@gmail.com>
2024-01-25 16:40:44 +0900

    feat: 02 작업 내역

---
+++ 02.Rendering/05/Sujin/view/counter.js
@@ -0,0 +1,16 @@
+const Counter = (todos) => {
+  const notCompleted = todos.filter((todo) => !todo.completed);
+
+  const { length } = notCompleted;
+  if (length === 1) {
+    return '1 Item left';
+  }
+
+  return `${length} Items left`;
+};
+
+export default (targetElement, { todos }) => {
+  const newCounter = targetElement.cloneNode(true);
+  newCounter.textContent = Counter(todos);
+  return newCounter;
+};
