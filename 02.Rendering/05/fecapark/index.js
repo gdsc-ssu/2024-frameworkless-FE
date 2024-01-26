4a6d34eb09c1ac9f9d18e525bafed8f9dbc34821
fecapark <fecapark@gmail.com>
2024-01-26 18:57:52 +0900

    feat: index.js - 일정 시간마다 재렌더링 코드 구현 완료

--- 02.Rendering/05/fecapark/index.js
+++ 02.Rendering/05/fecapark/index.js
@@ -1,14 +1,32 @@
 import getTodos from './getTodos.js';
+import counterView from './view/counter.js';
+import todosView from './view/todos.js';
+import filtersView from './view/filters.js';
+import { add, renderRoot } from './registry.js';
 
+const REFRESH_STATE_INTERVAL = 500;
 const state = {
   todos: getTodos(),
   currentFilter: 'All',
 };
 
-const main = document.querySelector('.todoapp');
+const render = () => {
+  window.requestAnimationFrame(() => {
+    const staleRoot = document.querySelector('.todoapp');
+    const freshRoot = renderRoot(staleRoot, state);
+    staleRoot.replaceWith(freshRoot);
+  });
+};
 
-window.requestAnimationFrame(() => {
-  // appView() 대신 registry.js 의 코드를 활용해보세요.
-  // const newMain = appView(main, state);
-  // main.replaceWith(newMain);
-});
+const renderWithNewState = () => {
+  state.todos = getTodos();
+  render();
+};
+
+window.onload = () => {
+  add('todos', todosView);
+  add('counter', counterView);
+  add('filters', filtersView);
+
+  setInterval(renderWithNewState, REFRESH_STATE_INTERVAL);
+};
