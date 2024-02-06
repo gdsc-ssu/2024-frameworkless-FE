678f0ac7b19959662f2e4559b39183653c6d1fe6
ilwoo <ooo952@naver.com>
2024-02-06 17:01:22 +0900

    복습 주석 추가

--- 02-Rendering/05-virtual-DOM/ilwoo/view/counter.js
+++ 02-Rendering/05-virtual-DOM/ilwoo/view/counter.js
@@ -1,5 +1,7 @@
 const getTodoCount = (todos) => {
+  // todos = [{text : 랜덤문자열, completed: true off false}]로 리턴
   const notCompleted = todos.filter((todo) => !todo.completed);
+  // notCompleted = completed가 false인 요소들만 모아놓은 배열
 
   const { length } = notCompleted;
   if (length === 1) {
