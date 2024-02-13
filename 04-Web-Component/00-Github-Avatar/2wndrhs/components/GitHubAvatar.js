54c569807bd76874f9190938d06243cb26b96bf8
2wndrhs <2wndrhs@gmail.com>
2024-02-13 20:25:08 +0900

    chore: boilerplate 코드 복사

---
+++ 04-Web-Component/00-Github-Avatar/2wndrhs/components/GitHubAvatar.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+export const ERROR_IMAGE = 'https://cdn-icons-png.flaticon.com/512/1304/1304038.png';
+const LOADING_IMAGE = 'https://cdn-icons-png.flaticon.com/512/3305/3305803.png';
+const AVATAR_LOAD_COMPLETE = 'AVATAR_LOAD_COMPLETE';
+const AVATAR_LOAD_ERROR = 'AVATAR_LOAD_ERROR';
+
+export const EVENTS = {
+  AVATAR_LOAD_COMPLETE,
+  AVATAR_LOAD_ERROR,
+};
+
+const getGitHubAvatarUrl = async (user) => {
+  if (!user) {
+    return null;
+  }
+
+  const url = `https://api.github.com/users/${user}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+  return data.avatar_url;
+};
+
+export default class GitHubAvatar extends HTMLElement {}
