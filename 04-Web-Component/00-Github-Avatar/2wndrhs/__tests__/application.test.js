0d89eb32899d4fdd5ccf26c42e20b2c9fe3345f8
이중곤 <2wndrhs@gmail.com>
2024-02-19 15:31:27 +0900

    [중곤] 04. 웹 구성 요소 - GitHubAvatar (#25)
    * chore: boilerplate 코드 복사
    
    * feat: getter/setter를 이용해 `GitHubAvatar` 요소에 `user` 속성 구현
    
    * feat: API 요청 전 LOADING_IMAGE를 표시하도록 구현
    
    * feat: API 요청 결과에 따라 `GitHubAvatar`의 컨텐츠가 달라지는 기능 구현
    
    * feat: API 요청 성공 시 `AVATAR_LOAD_COMPLETE` 커스텀 이벤트를 발생시키는 기눙 구현
    
    * feat: API 요청 실패 시 `AVATAR_LOAD_ERROR` 이벤트를 발생시키는 기능 구현
    
    * feat: `user` 속성이 변경되면 `GitHubAvatar`의 이미지가 변경되도록 하는 기능 구현
    
    * test: `user` 속성이 변경되면 `img` 요소의 `src` 속성이 변경되는지 확인하는 테스트 코드 작성

---
+++ 04-Web-Component/00-Github-Avatar/2wndrhs/__tests__/application.test.js
@@ -0,0 +1,132 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { jest } from '@jest/globals';
+import GitHubAvatar, { EVENTS, ERROR_IMAGE } from '../components/GitHubAvatar';
+
+describe('GitHubAvatar', () => {
+  beforeAll(() => {
+    customElements.define('github-avatar', GitHubAvatar);
+  });
+
+  let element;
+
+  beforeEach(() => {
+    // requestAnimationFrame을 모킹
+    jest.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => cb());
+    // GithubAvatar 사용자 정의 요소 생성
+    element = new GitHubAvatar();
+    // body에 사용자 정의 요소 추가
+    document.body.appendChild(element);
+  });
+
+  afterEach(() => {
+    // requestAnimationFrame 모킹 제거
+    window.requestAnimationFrame.mockRestore();
+    // body에서 사용자 정의 요소 제거
+    document.body.removeChild(element);
+  });
+
+  it('`user` 속성이 올바르게 설정되어야 한다', () => {
+    const user = 'octocat';
+    element.user = user;
+    expect(element.getAttribute('user')).toBe(user);
+  });
+
+  it('정상적인 `user` 속성이 설정되면 AVATAR_LOAD_COMPLETE 이벤트를 발생시킨다', async () => {
+    const mockUser = 'mockUser';
+    const mockAvatarUrl = 'https://mock.url/avatar.png';
+
+    // avatar_url을 반환하는 fetch를 모킹
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ avatar_url: mockAvatarUrl }),
+      }),
+    );
+
+    // AVATAR_LOAD_COMPLETE 이벤트 리스너를 등록
+    const loadCompleteHandler = jest.fn();
+    element.addEventListener(EVENTS.AVATAR_LOAD_COMPLETE, loadCompleteHandler);
+
+    // user 속성을 설정
+    element.user = mockUser;
+    element.connectedCallback();
+
+    // 모든 비동기 작업이 완료될 때까지 대기
+    await new Promise((resolve) => {
+      setTimeout(resolve, 0);
+    });
+
+    // AVATAR_LOAD_COMPLETE 이벤트가 발생하였는지 확인
+    expect(loadCompleteHandler).toHaveBeenCalled();
+
+    // img 요소의 src 속성이 변경되었는지 확인
+    const img = element.querySelector('img');
+    expect(img.src).toBe(mockAvatarUrl);
+  });
+
+  it('비정상적인 `user` 속성이 설정되면 AVATAR_LOAD_ERROR 이벤트를 발생시킨다', async () => {
+    const mockUser = 'mockUser';
+
+    // 에러를 던지는 fetch를 모킹
+    global.fetch = jest.fn().mockImplementation(() => Promise.reject(new Error('Not Found')));
+
+    // AVATAR_LOAD_ERROR 이벤트 리스너를 등록
+    const loadErrorHandler = jest.fn();
+    element.addEventListener(EVENTS.AVATAR_LOAD_ERROR, loadErrorHandler);
+
+    // user 속성을 설정
+    element.user = mockUser;
+    element.connectedCallback();
+
+    // 모든 비동기 작업이 완료될 때까지 대기
+    await new Promise((resolve) => {
+      setTimeout(resolve, 0);
+    });
+
+    // AVATAR_LOAD_ERROR 이벤트가 발생하였는지 확인
+    expect(loadErrorHandler).toHaveBeenCalled();
+
+    // img 요소의 src 속성이 변경되었는지 확인
+    const img = element.querySelector('img');
+    expect(img.src).toBe(ERROR_IMAGE);
+  });
+
+  it('`user` 속성이 변경되면 img 요소의 src 속성이 변경되어야 한다', async () => {
+    let mockUser = 'mockUser';
+    let mockAvatarUrl = 'https://mock.url/avatar.png';
+
+    // avatar_url을 반환하는 fetch를 모킹
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ avatar_url: mockAvatarUrl }),
+      }),
+    );
+
+    // user 속성을 설정
+    element.user = mockUser;
+    element.connectedCallback();
+
+    // 모든 비동기 작업이 완료될 때까지 대기
+    await new Promise((resolve) => {
+      setTimeout(resolve, 0);
+    });
+
+    mockUser = 'newMockUser';
+    mockAvatarUrl = 'https://mock.url/newAvatar.png';
+
+    // user 속성을 변경
+    element.user = mockUser;
+    element.connectedCallback();
+
+    // 모든 비동기 작업이 완료될 때까지 대기
+    await new Promise((resolve) => {
+      setTimeout(resolve, 0);
+    });
+
+    // img 요소의 src 속성이 변경되었는지 확인
+    const img = element.querySelector('img');
+    expect(element.getAttribute('user')).toBe(mockUser);
+    expect(img.src).toBe(mockAvatarUrl);
+  });
+});
---
+++ 04-Web-Component/00-Github-Avatar/2wndrhs/components/GitHubAvatar.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-vars */
+export const ERROR_IMAGE = 'https://cdn-icons-png.flaticon.com/512/1304/1304038.png';
+const LOADING_IMAGE = 'https://cdn-icons-png.flaticon.com/512/3305/3305803.png';
+const AVATAR_LOAD_COMPLETE = 'AVATAR_LOAD_COMPLETE';
+const AVATAR_LOAD_ERROR = 'AVATAR_LOAD_ERROR';
+
+export const EVENTS = {
+  AVATAR_LOAD_COMPLETE,
+  AVATAR_LOAD_ERROR,
+};
+
+const getGitHubAvatarUrl = async (user) => {
+  if (!user) {
+    return null;
+  }
+
+  const url = `https://api.github.com/users/${user}`;
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+  return data.avatar_url;
+};
+
+export default class GitHubAvatar extends HTMLElement {
+  constructor() {
+    super();
+    this.url = LOADING_IMAGE;
+  }
+
+  static get observedAttributes() {
+    return ['user'];
+  }
+
+  get user() {
+    return this.getAttribute('user');
+  }
+
+  set user(value) {
+    this.setAttribute('user', value);
+  }
+
+  onAvatarLoadComplete() {
+    this.dispatchEvent(
+      new CustomEvent(AVATAR_LOAD_COMPLETE, {
+        detail: {
+          avatar: this.url,
+        },
+      }),
+    );
+  }
+
+  onAvatarLoadError(error) {
+    this.dispatchEvent(
+      new CustomEvent(AVATAR_LOAD_ERROR, {
+        detail: {
+          error,
+        },
+      }),
+    );
+  }
+
+  render() {
+    window.requestAnimationFrame(() => {
+      this.innerHTML = `<img src="${this.url}" alt="GitHub Avatar" />`;
+    });
+  }
+
+  async loadAvatar() {
+    try {
+      this.url = await getGitHubAvatarUrl(this.user);
+      this.onAvatarLoadComplete();
+    } catch (error) {
+      this.url = ERROR_IMAGE;
+      this.onAvatarLoadError(error);
+    }
+
+    this.render();
+  }
+
+  connectedCallback() {
+    this.render();
+    this.loadAvatar();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.loadAvatar();
+    }
+  }
+}
